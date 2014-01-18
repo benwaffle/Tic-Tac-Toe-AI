d2a03be20c08f7bea06ea5bdae5597e69c1d5852
Ben Iofel <iofelben@gmail.com>
2014-01-18 00:23:31 -0500

    added java gui

---
+++ GUI.java
@@ -0,0 +1,102 @@
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+
+public class GUI {
+	private JFrame frame;
+	private String[] board = {" "," "," "," "," "," "," "," "," "};;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public GUI() {
+		initialize();
+	}
+
+
+	private void initialize() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
+		frame = new JFrame();
+		frame.setBounds(100, 100, 300, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(3, 3, 0, 0));
+		
+		JButton[] buttons = new JButton[9];
+		for(int i = 0; i < 9; i++){
+			buttons[i] = new JButton("");
+			buttons[i].setFont(new Font("Arial", Font.PLAIN, 30));
+			buttons[i].addActionListener(new ActionListenerWithID(buttons, i, board));
+			frame.getContentPane().add(buttons[i]);
+		}
+	}
+}
+
+class ActionListenerWithID implements ActionListener {
+	int id = -1;
+	String[] board;
+	JButton[] guiboard;
+	
+	public ActionListenerWithID(JButton[] guiboard, int id, String[] board) {
+		this.guiboard = guiboard;
+		this.id = id;
+		this.board = board;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(board[id] == " ")
+			set("X");
+		else
+			JOptionPane.showMessageDialog(null, "Box taken, choose a different box");
+	}
+	
+	private void set(String c){
+		board[id] = c;
+		guiboard[id].setText("X");
+		
+		int comp = Util.chooseMove(board, Util.COMPUTER).move;
+		if (comp != 32){
+			board[comp] = "O";
+			guiboard[comp].setText("O");
+		}
+		
+		Util.printTTTBoard(board);
+		System.out.println();
+		
+		if(Util.isBoardFull(board) && Util.checkWin(board) == " "){
+			JOptionPane.showMessageDialog(null, "Draw");
+			reset();
+		} else if(Util.checkWin(board) == "O"){
+			JOptionPane.showMessageDialog(null, "Computer won");
+			reset();
+		}
+	}
+	private void reset(){
+		Arrays.fill(board, " ");
+		for(JButton jb : guiboard) jb.setText("");
+	}
+}
--- main.c
+++ main.c
@@ -16,138 +16,121 @@
  board = ['0','1','2','3','4','5','6','7','8'];
  */
 #include <stdio.h>
-#include <stdlib.h>
 
+#define COMPUTER 1
+#define HUMAN 0
+#define SIZE 9
 //current game board
-char board[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+ char board[SIZE] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
 
-typedef struct best{
-    int move;
-    int score;
-} best;
+ typedef struct best{
+ 	int move;
+ 	int score;
+ } best;
 
-int numOfCharOnBoard(char check, char board[]){
-    int count = 0;
-    for(int i=0;i<9;i++){
-        if(board[i]==check){
-            count++;
-        }
-    }
-    return count;
-}
+ int numOfCharOnBoard(char check, char board[]){
+ 	int count = 0, i = 0;
+ 	for(; i < SIZE; i++) if(board[i] == check) count++;
+ 		return count;
+ }
 
-void printArray(char array[]){
-    for(int i=0;i<=sizeof(*array)/sizeof(array[0]);i++) printf("%c ",array[i]);
-}
+ void printArray(char array[]){
+ 	int i = 0;
+ 	for(; i <= sizeof(array)/sizeof(array[0]); i++) printf("%c ",array[i]);
+ }
 
 char checkwin(char board[]){
-    if(board[0] == board[1] && board[0] == board[2]) return board[0];
-    else if(board[3] == board[4] && board[3] == board[5]) return board[3];
-    else if(board[6] == board[7] && board[6] == board[8]) return board[6];
-    else if(board[0] == board[3] && board[0] == board[6]) return board[0];
-    else if(board[1] == board[4] && board[1] == board[7]) return board[1];
-    else if(board[2] == board[5] && board[2] == board[8]) return board[2];
-    else if(board[0] == board[4] && board[0] == board[8]) return board[0];
-    else if(board[2] == board[4] && board[2] == board[6]) return board[2];
-    else return ' ';
+	if(board[0] == board[1] && board[0] == board[2]) return board[0];
+	else if(board[3] == board[4] && board[3] == board[5]) return board[3];
+	else if(board[6] == board[7] && board[6] == board[8]) return board[6];
+	else if(board[0] == board[3] && board[0] == board[6]) return board[0];
+	else if(board[1] == board[4] && board[1] == board[7]) return board[1];
+	else if(board[2] == board[5] && board[2] == board[8]) return board[2];
+	else if(board[0] == board[4] && board[0] == board[8]) return board[0];
+	else if(board[2] == board[4] && board[2] == board[6]) return board[2];
+	else return ' ';
 }
 
 best chooseMove(char board[], int side){
-    best myBest;
-    best reply;
-    
-    //-----------------------------------
-    
+	best myBest;
+	best reply;
+
     //return terminal node value
     if(checkwin(board) == 'X'){         //X has won (human)
-        myBest.move = ' ';
+    	myBest.move = ' ';
         myBest.score = -1;              //-1 for computer
         return myBest;
-    }
-    else if(checkwin(board) == 'O'){    //O has won (computer)
-        myBest.move = ' ';
+    } else if(checkwin(board) == 'O'){    //O has won (computer)
+    	myBest.move = ' ';
         myBest.score = 1;               //+1 for computer
         return myBest;
-    }
-    else if(numOfCharOnBoard(' ', board)==0){ //no empty slots, board is full
-        myBest.move = ' ';
+    } else if(numOfCharOnBoard(' ', board) == 0){ //no empty slots, board is full
+    	myBest.move = ' ';
         myBest.score = 0;               //its a draw
         return myBest;
     }
     
-    //-----------------------------------
-    
-    
     //set the score to be worst possible
-    if(side==1){    //side 1 = O , or computer
-        myBest.score = -2;
-    } else {        //side 0 = X , or human
-        myBest.score = 2;
-    }
-    
-    //-----------------------------------
+    myBest.score = (side == COMPUTER ? -2 : 2);
     
     //recursively call chooseMove() until you get to a leaf
-    for(int i=0;i<=(sizeof(board)/sizeof(board[0]));i++){   //for each item in the board
-        if(board[i]==' '){                                      //if it is empty
-            if (side==1){                                           //if computer turn
-                board[i] = 'O';                                         //place O
-                reply = chooseMove(board, 0);                         //recursive part (0 = X = human)
-                board[i] = ' ';                                         //undo move
-            } else {                                                //else (human turn)
-                board[i] = 'X';                                         //place X
-                reply = chooseMove(board, 1);                         //recursive part (1 = O = computer)
-                board[i] = ' ';                                         //undo move
+    int i = 0;
+    for(; i <= SIZE; i++) if(board[i] == ' '){ //for each available spot
+    	if (side == COMPUTER){
+    		board[i] = 'O';
+                reply = chooseMove(board, 0); //recurse
+                board[i] = ' ';
+            } else {   
+            	board[i] = 'X';
+                reply = chooseMove(board, 1); //recurse
+                board[i] = ' ';
             }
-            if ((side==1 && (reply.score>myBest.score)) || (side==0 && (reply.score<myBest.score))){    //if computer turn and we got a better move or
-                myBest.move = i;                                                                        //if human turn and has move worse for computer
-                myBest.score = reply.score;                                                             //set the move and the score (minimax value)
+            // if we found a better score
+            if ((side == COMPUTER && reply.score > myBest.score) || (side == HUMAN && reply.score < myBest.score)){   
+            	myBest.move = i;
+                myBest.score = reply.score; //set the move and the score (minimax value)
             }
         }
-    }
-    return myBest;                                      //return the best move
+    return myBest; //return the best move
 }
 
 void printTTTBoard(char board[]){
-    for(int i=0;i<=(sizeof(board)/sizeof(board[0]));i++){
-        if(board[i] == ' ') printf(" %i ",i);
-        else printf(" %c ",board[i]);
-        if(i != 2 && i != 5 && i != 8) printf("|");
-        if(i == 2 || i == 5) printf("\n------------\n");
-    }
-    printf("\n");
+	int i = 0;
+	for(; i <= SIZE; i++){
+		if(board[i] == ' ') printf(" %i ",i);
+		else printf(" %c ", board[i]);
+		if(i != 2 && i != 5 && i != 8) printf("|");
+		if(i == 2 || i == 5) printf("\n------------\n");
+	}
+	printf("\n");
 }
 
 int isboardfull(char board[]){
-    for(int i=0;i<=(sizeof(board)/sizeof(board[0]));i++){
-        if (board[i] == ' ') {
-            return 0;      //loop through board until you encounter empty slot, then board isn't full --> return 0
-            break;         //break out of function
-        }
-    }
-    return 1;              //board has been looped through with out an empty slot found so return 1
+	int i = 0;
+	for(; i <= SIZE; i++) if(board[i] == ' ') return 0;
+	return 1;
 }
 
-int main(int argc, const char * argv[]){
-    char move[] = "";
-    printf("Welcome to Ben's Tic Tac Toe. (You can't win)\nBegin by placing your first move:\n");
-    while(checkwin(board) == ' ' && isboardfull(board) == 0){
-            /* while loop should run for the duration of a game game ends when board is full(draw) OR x or o has won. game continue if there is an empty slot and there's no winner*/
-        printf("\n");
-        printTTTBoard(board);                       //print the board
-        printf("enter your move: ");                //prompt the user
-        scanf("%s",move);                           //get input
-        if(board[atoi(move)] == ' '){               //if slot is empty
-            board[atoi(move)] = 'X';                //place user move
-            int compmove = chooseMove(board, 1).move;
-            board[compmove] = 'O'; //place computer move
-        } else {
-            printf("that spot is occupied\n");
-        }
-    }
-    printTTTBoard(board);
-    if(checkwin(board) == 'X') printf("you won!\n");
-    else if(checkwin(board) == 'O') printf("you lost!\n");
-    else if(checkwin(board) == ' ') printf("the game was a draw");
-    return 0;
+int main(int argc, char *argv[]){
+	printf("Welcome to Ben's Tic Tac Toe. (You can't win)\n");
+
+	int move;
+	while(checkwin(board) == ' ' && isboardfull(board) == 0){
+		printf("\n");
+		printTTTBoard(board);
+		printf("enter your move: ");
+		scanf("%d", &move);
+
+		// if(board[move] == ' '){
+		// 	board[move] = 'X';
+		// 	board[chooseMove(board, 1).move] = 'O';
+		// } else printf("that spot is occupied\n");
+	}
+
+	printTTTBoard(board);
+	if (checkwin(board) == 'X') printf("you won!\n");
+	else if (checkwin(board) == 'O') printf("you lost!\n");
+	else if (checkwin(board) == ' ') printf("the game was a draw");
+
+	return 0;
 }
